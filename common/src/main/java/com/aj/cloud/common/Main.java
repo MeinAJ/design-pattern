f2277d5b2f4a70aeff83a3c2216ed9e51433e121
安军 <Jun.An3@geely.com>
2022-05-11 10:30:51 +0800

    update

---
+++ common/src/main/java/com/aj/cloud/common/Main.java
@@ -0,0 +1,15 @@
+package com.aj.cloud.common;
+
+/**
+ * todo 类描述
+ *
+ * @author Jun.An3
+ * @date 2022/05/11
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
